8461b6407ceece5aaed834819cf1d9c1c0ecd4e9
KeyDaynessX <118815990+KeyDaynessX@users.noreply.github.com>
2023-02-19 23:39:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,19 +8,6 @@ import Products from './components/Pages/Products';
 import SignUp from './components/Pages/SignUp';
 
 function App() {
-  fetch('https://example.com/api/data')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
   return <>
   <Routes>
     <Route path='/' element={<Navbar/>}>
